e6a5236e9dda7d07a627bf5f761c2b0a5aab6a9c
Stefano Mauceri <48856268+spaghettix@users.noreply.github.com>
2021-10-11 10:10:01 +0100

    Add files via upload

--- src/DistPAE.py
+++ src/DistPAE.py
@@ -2,7 +2,7 @@
 
 
 """
-ABOUT: DENSE AUTO-ENCODER
+ABOUT: DENSE DISTANCE PRESERVING AUTO-ENCODER
 """
 
 
@@ -22,8 +22,9 @@ import tensorflow as tf
 tf.keras.backend.set_floatx('float32')
 from nn_utils import AEBase, LossHistory
 from tensorflow.keras import Model, Sequential
-from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.layers import Dense, Input, Lambda
 from tensorflow.keras.callbacks import ReduceLROnPlateau
+from tensorflow_addons.losses.metric_learning import pairwise_distance as PD
 
 
 
@@ -33,7 +34,7 @@ from tensorflow.keras.callbacks import ReduceLROnPlateau
 
 
 
-class AE(AEBase):
+class DistPAE(AEBase):
 
 
 
@@ -46,18 +47,18 @@ class AE(AEBase):
                  optimizer='Adam',
                  lr=0.001,
                  seed=None,
+                 loss_weights=[1., 1.],
                  **kwargs):
 
-        input_size = int(input_size)
+        self.input_size = int(input_size)
 
         if latent_dim is None:
             latent_dim = int(tf.math.ceil(tf.math.sqrt(tf.convert_to_tensor(input_size, dtype='float32'))))
 
         if structure is None:
-            structure = np.geomspace(input_size, latent_dim, n_layers+1).astype(int)
-
-            if structure[0] != input_size:
-                structure[0] = input_size
+            structure = np.geomspace(self.input_size, latent_dim, n_layers+1).astype(int)
+            if structure[0] != self.input_size:
+                structure[0] = self.input_size
             if structure[-1] != latent_dim:
                 structure[-1] = latent_dim
 
@@ -69,17 +70,21 @@ class AE(AEBase):
 
         self.optimizer = getattr(tf.keras.optimizers, optimizer)(learning_rate=lr)
 
-        self.encoder_input = Input((input_size,), name='layer_0.in')
+        self.encoder_input = Input((self.input_size,), name='layer_0.in')
         self.decoder_input = Input((self.structure[-1],), name='layer_0.out')
+        self.distance_matrix_input = Input((None,), name='distance_matrix')
 
         self.encoder = self.build_encoder()
         self.decoder = self.build_decoder()
+        self.dp_loss = Lambda(PD, name='dp_loss')(self.encoder(self.encoder_input))
 
-        self.model = Model(inputs=self.encoder_input,
-                           outputs=self.decoder(self.encoder(self.encoder_input)))
+        self.model = Model(inputs=[self.encoder_input, self.distance_matrix_input],
+                            outputs=[self.decoder(self.encoder(self.encoder_input)), self.dp_loss])
 
-        self.model.compile(loss='mse',
-                           optimizer=self.optimizer)
+        self.model.compile(loss=['mse', 'mse'],
+                            loss_weights=loss_weights,
+                            optimizer=self.optimizer)
+
         self.loss_tracker = LossHistory()
 
         self.lr_tracker = ReduceLROnPlateau(monitor='loss',
@@ -90,7 +95,6 @@ class AE(AEBase):
                                             verbose=False)
 
         self.call_backs = [self.loss_tracker, self.lr_tracker]
-
 
 
 
@@ -141,23 +145,43 @@ class AE(AEBase):
 
 
 
-    def fit(self, X, epochs, batch_size=None):
+    def fit(self, X, distance_matrix, epochs, batch_size=None):
         if batch_size is None:
             batch_size = X.shape[0]
 
-        self.model.fit(x=X,
-                       y=X,
+        def generator(X, dist_matrix, batch_size):
+            nsamples = X.shape[0]
+            while True:
+                ix = tf.random.uniform(shape=(batch_size,),
+                                       minval=0,
+                                       maxval=nsamples,
+                                       dtype=tf.int32)
+                x = tf.gather(X, indices=ix, axis=0)
+                dm = tf.gather_nd(dist_matrix, indices=tf.stack(tf.meshgrid(ix, ix), axis=-1))
+                yield (x, dm), (x, dm)
+
+        Data = tf.data.Dataset.from_generator(generator,
+                                              ((tf.float32, tf.float32), (tf.float32, tf.float32)),
+                                              ((tf.TensorShape([None,self.input_size]), tf.TensorShape([batch_size,batch_size])), (tf.TensorShape([None,self.input_size]), tf.TensorShape([batch_size,batch_size]))),
+                                              args=[X, distance_matrix, batch_size])
+
+        steps = int(tf.math.ceil(X.shape[0]/batch_size))
+        self.model.fit(Data,
                        epochs=epochs,
-                       shuffle=True,
+                       steps_per_epoch=steps,
                        callbacks=self.call_backs,
+                       shuffle=False,
                        validation_data=None,
-                       verbose=False)
+                       verbose=False,
+                       use_multiprocessing=False)
 
 
 
     def loss_history(self):
         history = self.loss_tracker.get_history()
-        return history.loss.values
+        return {'total_loss': history.loss.values,
+                'r_loss': history.Decoder_loss.values,
+                'dp_loss': history.dp_loss_loss.values}
 
 
 
@@ -170,8 +194,11 @@ class AE(AEBase):
 if __name__ == '__main__':
 
 
+
     import os
     import matplotlib.pyplot as plt
+    from dissimilarity import dissimilarity
+    from scipy.spatial.distance import cdist
 
 
     # IMPORT DATA
@@ -194,30 +221,41 @@ if __name__ == '__main__':
     X_test_pos = X_test[(Y_test == class_)]
     X_test_neg = X_test[(Y_test != class_)]
 
+    diss = 'DTW'
+    D = dissimilarity()
+    D = getattr(D, diss)
+    DM = cdist(X_train_pos, X_train_pos, metric=D)
+    DM = tf.linalg.normalize(DM, ord='euclidean')[0]
+
+
     # MODEL
-    model = AE(input_size=X_train_pos.shape[1],
-               n_layers=3,
-               latent_dim=2,
-               optimizer='Adam',
-               activation='tanh',
-               lr=0.001)
+    model = DistPAE(input_size=X_train_pos.shape[1],
+                    n_layers=3,
+                    latent_dim=2,
+                    optimizer='Adam',
+                    activation='tanh',
+                    lr=0.001,
+                    loss_weights=[1., 1.])
 
 
     print('ENC structure: ', model.structure)
     print('Training Samples: ', X_train_pos.shape[0])
-
     model.show_config(model.encoder)
     model.show_config(model.decoder)
 
+
     # FIT
     model.fit(X_train_pos,
-              epochs=500,
+              distance_matrix=DM,
+              epochs=2000,
               batch_size=4)
 
 
     # PLOT LOSS
     loss = model.loss_history()
-    plt.plot(loss, '-k', label='total')
+    plt.plot(loss['total_loss'], '-k', label='total')
+    plt.plot(loss['r_loss'], '-r', label='rec')
+    plt.plot(loss['dp_loss'], '-b', label='dist')
     plt.title('Training Loss')
     plt.legend()
     plt.show()
